d19b77130777d59f4da3ab142d90de3b9d0eec0d
Jscribel <jackscrib2004@gmail.com>
2024-03-28 13:21:49 -0400

    -Cleaned up UI(not final)

--- composeApp/src/commonMain/kotlin/views/TunerView.kt
+++ composeApp/src/commonMain/kotlin/views/TunerView.kt
@@ -2,22 +2,40 @@ package views
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import moe.tlaster.precompose.viewmodel.viewModel
+import viewModels.GeneralViewModel
+import viewModels.TunerViewModel
+import kotlin.math.roundToInt
 
 @Composable
-fun tunerView() {
+fun tunerView(generalViewModel: GeneralViewModel) {
+    val tunerViewModel = viewModel(modelClass = TunerViewModel::class){
+        TunerViewModel()
+    }
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Tuner Page")
-        Text(text = "Pitch")
-        Text(text = "0")
+        FloatingActionButton(
+            onClick = {
+
+            },
+            backgroundColor = generalViewModel.highlightColor,
+            contentColor = generalViewModel.foregroundColor
+        ){
+            Text(
+                text = tunerViewModel.pitch.toString()
+            )
+        }
+        Text(text = tunerViewModel.pitch.getOffset().roundToInt().toString())
 
     }
 }
--- composeApp/src/commonMain/kotlin/views/TuningsView.kt
+++ composeApp/src/commonMain/kotlin/views/TuningsView.kt
@@ -9,37 +9,60 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.jscribel.runetune.Pitch
 import moe.tlaster.precompose.viewmodel.viewModel
 import viewModels.GeneralViewModel
+import viewModels.TuningsViewModel
+import kotlin.math.roundToInt
 
-val pitches : List<String> = listOf("D", "E", "A", "D", "G", "B", "E")//test hardcode
+val pitches : List<Pitch> = listOf(Pitch("E", 82.0, 2), Pitch("A", 110.0, 2), Pitch("D", 147.0, 3), Pitch("G", 196.0, 3), Pitch("B", 247.0, 3), Pitch("E", 330.0, 4))//test hardcode
 
 @Composable
-fun tuningsView() {
-    val generalViewModel = viewModel(modelClass = GeneralViewModel::class){
-        GeneralViewModel()
+fun tuningsView(generalViewModel: GeneralViewModel) {
+    val tuningsViewModel = viewModel(modelClass = TuningsViewModel::class){
+        TuningsViewModel()
     }
 
     Column(
         modifier = Modifier.fillMaxWidth().fillMaxHeight(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Text(text = "Tuning Sets Page")
-        Text(text = "Pitch")
-        Text(text = "0")
+
+        //display
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ){
+            FloatingActionButton(
+                onClick = {
+
+                },
+                backgroundColor = generalViewModel.highlightColor,
+                contentColor = generalViewModel.foregroundColor
+            ) {
+                Text(
+                    text = tuningsViewModel.pitch.toString()
+                )
+            }
+            Text(text = tuningsViewModel.pitch.getOffset().roundToInt().toString())
+        }
+
+        //Pitches
         LazyRow(
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly
         ){
             items((pitches.size + 2)/3){indexColumn ->
                 LazyColumn(
-                    modifier = Modifier.fillMaxHeight(),
+                    //modifier = Modifier.fillMaxHeight(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.SpaceEvenly
                 ){
@@ -50,13 +73,13 @@ fun tuningsView() {
                     items(countButton){indexButton ->
                         Button(
                             onClick = {
-
+                                tuningsViewModel.changePitch(pitches[(indexColumn * 3 + indexButton)])
                             },
                             shape = CircleShape,
                             colors = ButtonDefaults.buttonColors(backgroundColor = generalViewModel.highlightColor, contentColor = generalViewModel.foregroundColor)
                         ) {
                             Text(
-                                text = pitches[(indexColumn * 3 + indexButton)]
+                                text = pitches[(indexColumn * 3 + indexButton)].toString()
                             )
                         }
                     }

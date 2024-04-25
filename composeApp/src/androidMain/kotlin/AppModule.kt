1ae3a9e6fbc02b561c6be383f0dd91ea5262e271
Jscribel <jackscrib2004@gmail.com>
2024-04-25 08:42:57 -0400

    -Fixed database

--- composeApp/src/androidMain/kotlin/AppModule.kt
+++ composeApp/src/androidMain/kotlin/AppModule.kt
@@ -1,8 +1,5 @@
 import android.app.Activity
 import android.content.Context
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import be.tarsos.dsp.AudioEvent
 import be.tarsos.dsp.filters.LowPassFS
 import be.tarsos.dsp.io.android.AudioDispatcherFactory
@@ -13,10 +10,13 @@ import com.jscribel.Database
 import dataSources.PitchDataSource
 import dataSources.SettingDataSource
 import dataSources.TuningDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import viewModels.GeneralViewModel
 
-actual class AppModule(private val context : Context) {
-    private val database by lazy {
+actual class AppModule(private val context : Context){
+
+    private val database by lazy{
         Database(
             driver = DatabaseDriverFactory(context).create()
         )
@@ -27,14 +27,14 @@ actual class AppModule(private val context : Context) {
     actual fun provideTuningDataSource() = TuningDataSource(database)
 
     actual fun startPitchDetector(generalViewModel: GeneralViewModel){//add permission checker
-        val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(4096, 4096, 0)
+        val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 2048, 0)
         val pitchDetectionHandler = PitchDetectionHandler{ result : PitchDetectionResult, e : AudioEvent ->
             val pitchResult = result.pitch
             (context as Activity).runOnUiThread {
                 generalViewModel.changePitch(PitchDataSource(database).getClosest(pitchResult.toDouble()))
             }
         }
-        val pitchProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 4096f, 4096, pitchDetectionHandler)
+        val pitchProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050f, 2048, pitchDetectionHandler)
         dispatcher.addAudioProcessor(LowPassFS(50f, 10000f))
         dispatcher.addAudioProcessor(pitchProcessor)
         Thread(dispatcher, "Pitch Detector").start()
--- composeApp/src/commonMain/kotlin/dataSources/PitchDataSource.kt
+++ composeApp/src/commonMain/kotlin/dataSources/PitchDataSource.kt
@@ -13,22 +13,13 @@ class PitchDataSource(private val database: Database){
     }
 
     fun getClosest(frequency : Double) : Pitch {
-        val pitch = queries.getByFrequency(frequency).executeAsOneOrNull()
         return if(frequency < 0){
             Pitch("N", -1, -1.0, -1.0)
         }
-        else if(pitch == null){
-            val pitchHigh = queries.getNextByFrequency(frequency).executeAsOneOrNull()
-            val pitchLow = queries.getPrevByFrequency(frequency).executeAsOneOrNull()
-
-            if(pitchHigh == null || pitchLow != null && pitchHigh.frequency - frequency > frequency - pitchLow.frequency){
-                Pitch(pitchLow!!.note, pitchLow.octave, pitchLow.frequency, 1200 * log2(frequency/pitchLow.frequency))//why is !! needed
-            }
-            else{
-                Pitch(pitchHigh.note, pitchHigh.octave, pitchHigh.frequency, 1200 * log2(frequency/pitchHigh.frequency))// if else block for frequency
-            }
+        else{
+            val pitch = queries.getClosest(frequency).executeAsOne()
+            Pitch(pitch.note, pitch.octave, pitch.frequency, 1200 * log2(frequency/pitch.frequency))
         }
-        else Pitch(pitch.note, pitch.octave, pitch.frequency)
     }
 
     fun getAll() : List<Pitch>{
